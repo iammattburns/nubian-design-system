8661f27f964473d2093c7ad97a075a347e473969
Matt Burns <iammattburns@gmail.com>
2023-11-01 11:39:38 +0000

    refactor: button component to use styled-components

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,39 +3,32 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Button from "./Button";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Example/Button",
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: "color" },
   },
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
+  variant: "primary",
   label: "Button",
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
+  variant: "secondary",
   label: "Button",
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: "large",
-  label: "Button",
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: "small",
-  label: "Button",
+export const ButtonAsLink = Template.bind({});
+ButtonAsLink.args = {
+  as: "a",
+  href: "https://iammattburns.dev",
+  target: "_blank",
+  label: "Click me!",
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,34 +1,82 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
-interface ButtonProps {
-  primary?: boolean;
-  backgroundColor?: string;
-  size?: "small" | "medium" | "large";
+type AsButtonProps = {
+  as?: "button";
+};
+type AsAnchorProps = {
+  as?: "a";
+  href: string;
+  target?: "_blank" | "_self" | "_parent" | "_top";
+};
+type ButtonProps = {
+  variant?: "primary" | "secondary";
   label: string;
+  disabled?: boolean;
   onClick?: () => void;
-}
+} & (AsButtonProps | AsAnchorProps);
+
+type StyledButtonProps = {
+  variant?: "primary" | "secondary";
+};
+const StyledButton = styled.button<StyledButtonProps>(
+  ({ variant }) => css`
+    color: #fff;
+    border: none;
+    padding: 0.75rem 1.5rem;
+    border-radius: 6px;
+    font-size: 1.15rem;
+    &:hover {
+      cursor: pointer;
+    }
+
+    ${(() => {
+      switch (variant) {
+        case "primary":
+          return css`
+            background-color: #e74079;
+            color: #fff;
+            &:hover {
+              background-color: #dc3f73;
+            }
+          `;
+        case "secondary":
+          return css`
+            background-color: #fff;
+            color: #e74079;
+            border: 1px solid #e74079;
+            &:hover {
+              background-color: #f9f9f9;
+            }
+          `;
+        default:
+          return null;
+      }
+    })()}
+  `
+);
 
 const Button = ({
-  primary = false,
-  size = "medium",
-  backgroundColor,
+  as = "button",
+  variant = "primary",
   label,
+  disabled,
+  onClick,
   ...props
 }: ButtonProps) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+  const { href, target } = { ...props };
+
   return (
-    <button
-      type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
-      style={{ backgroundColor }}
-      {...props}
+    <StyledButton
+      as={as}
+      variant={variant}
+      href={href}
+      target={target}
+      disabled={disabled}
+      onClick={onClick}
     >
       {label}
-    </button>
+    </StyledButton>
   );
 };
 
